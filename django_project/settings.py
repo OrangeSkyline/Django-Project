e03ef3938ba8b96bc1591ddcdc8d8e264bd7998b
OrangeSkyline <thedeadlyboo999@gmail.com>
2026-09-17 17:03:16 -0600

    Created polls app, doing django documentation tutorial

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yh!h7#-j4gg-r(7=xxve#qddgqwbk_1@sq)8cjr%w)ql39ayb='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1","www.orangeskyline.com"]
+ALLOWED_HOSTS = ["127.0.0.1","https://www.orangeskyline.com"]
 
 
 # Application definition

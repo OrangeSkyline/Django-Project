c84727286cd1dfeff7f81baa77193d4e7aacf212
OrangeSkyline <thedeadlyboo999@gmail.com>
2026-09-21 13:20:40 -0600

    Moved about.html in new templates folder, and modified about urls & views file to allow the html file

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import about_view
 
 urlpatterns = [
-    path("", views.about, name="about"),
+    path("", about_view, name="about"),
 ]
